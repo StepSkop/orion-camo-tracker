6ca5facccbb2640e200e18d0ccce339b3e3ada12
StepSkop <74026500+StepSkop@users.noreply.github.com>
2022-11-08 13:00:58 +0100

    Added basic challenge requirements

--- weaponsUpdate.js
+++ weaponsUpdate.js
@@ -4,14 +4,14 @@ const weapons = require('./updatedWeapons.json')
 weapons.forEach(cat => {
     cat.guns.forEach(gun => {
         if (gun.name == 'X13 Auto') {
-            // gun.challenges[0]['requirements'] = 'Void and Light - Get 40 kills'
-            // gun.challenges[1]['requirements'] = 'Asphalt Digital - '
-            // gun.challenges[2]['requirements'] = 'Dark Echo - '
-            // gun.challenges[3]['requirements'] = 'Party Time - '
+            //gun.challenges[0]['requirements'] += 'Get 50 kills'
+            gun.challenges[1]['requirements'] += 'Get 10 hip fire kills'
+            gun.challenges[2]['requirements'] += 'Get 10 double kills'
+            gun.challenges[3]['requirements'] += 'Get 40 kills with Akimbo attachment'
         }
         gun.challenges.forEach(challenge => {
-            // if (challenge.name == 'Gold' && challenge.requirements == 'TBA') {
-            //     challenge['requirements'] = 'Gold - '
+            // if (challenge.name == 'Gold' && cat.category == 'BR') {
+            //     challenge['requirements'] += 'Get 3 kills without dying 10 times'
             // }
         });
     });
